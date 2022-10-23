d9d9ff6a6fc66b138a145d2082f0f0af90b96c7f
Arick45 <39058961+Arick45@users.noreply.github.com>
2022-10-23 10:08:11 +0300

    Home4(two projects)

---
+++ Home4/Home4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HomeTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Заполняем массив
+            Console.WriteLine("Укажите длинну массива");
+            Random rand = new Random();
+            int len;
+            int[] array;
+            try
+            {
+                len = int.Parse(Console.ReadLine());
+                array = new int[len];
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите целое число");
+                return;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Не положительная длинна");
+                return;
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = rand.Next(-1000, 1000) % 10;
+            }
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("-----------Масссив до изменения----------------");
+            foreach (var item in array)//распечатываем
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------------------------------");
+            // часть 1
+            int count = 0, zeroLastElIn = -1, countone = 0, sum = 0;
+            for (int i = 0; i < len; i++)//определение последнего нуля и сумма положительных элементов
+            {
+                if (array[i] > 0)
+                {
+                    count++;
+                }
+                if (array[i] == 0)
+                {
+                    zeroLastElIn = i;
+                }
+            }
+            if (zeroLastElIn == -1)
+            {
+                Console.WriteLine("Нету чисел равных нулю");
+            }
+            else
+            {
+                for (int i = zeroLastElIn + 1; i < len; i++)//сумма элементов после последнего нуля
+                {
+                    sum += array[i];
+                }
+                Console.WriteLine($"Последний нулевой элемент на позиции: {zeroLastElIn + 1}");
+                Console.WriteLine($"Cумма элементов массива, расположенных после последнего элемента,равного нулю: {sum}");
+            }
+            for (int i = 0; i < len; i++)//изменяем массив
+            {
+                if (array[i] <= 1)
+                {
+                    int temp = array[countone];
+                    array[countone] = array[i];
+                    array[i] = temp;
+                    countone++;
+                }
+            }
+            Console.WriteLine("----------------------------------------------");
+            // часть 2
+            Console.WriteLine($"Количество положительных чисел в массиве: {count}");
+            Console.WriteLine("-----------Масссив после изменения----------------");
+            foreach (var item in array)//распечатываем
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
